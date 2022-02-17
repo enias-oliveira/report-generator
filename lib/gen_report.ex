c58db777654afe9a2e8aa436a1c108f38834ae96
Enias Oliveira <eniasoliveira27@gmail.com>
2022-02-17 00:11:15 -0300

    feat: adds paralled report geneartion for multiple files

--- lib/gen_report.ex
+++ lib/gen_report.ex
@@ -5,9 +5,22 @@ defmodule GenReport do
     {:error, "Insira o nome de um arquivo"}
   end
 
+  def build(filename) when is_list(filename) do
+    filename
+    |> Task.async_stream(&build/1)
+    |> Enum.reduce(%{}, fn {:ok, report}, acc -> map_merge_recursion(report, acc) end)
+  end
+
   def build(filename) do
-    Parser.parse_file(filename)
+    filename
+    |> Parser.parse_file()
     |> Enum.reduce(%{}, &calculate_report(&1, &2))
+  end
+
+  defp map_merge_recursion(map1, map2) do
+    Map.merge(map1, map2, fn _, val1, val2 ->
+      if is_map(val1), do: map_merge_recursion(val1, val2), else: val1 + val2
+    end)
   end
 
   defp calculate_report([name, hours, _, month, year], report) do
